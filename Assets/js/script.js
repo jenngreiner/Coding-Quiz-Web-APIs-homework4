0a314b2d5d10496c81d4fa86f9d4a3aaaad4a743
Jenn Greiner <jenn.greiner1@gmail.com>
2021-01-20 16:15:03 -0700

    Created html and js files

---
+++ Assets/js/script.js
@@ -0,0 +1,13 @@
+Javascript quiz
+// main index
+    // View highscores button top left
+    // Timer top right
+    // wrong answers decrease time left
+
+    // final score displayed after all questions are answered
+    // final score is the number of seconds remaining on the timer
+    // enter initials in text field to save score
+        // Click save stores score and opens Highscore.html
+// Highscores live in separate HTML page; displays list of saved scores
+    // buttons to go back to quiz or clear scores
+    // scores are stored even if you refresh your browser
