0722229f388779fe607c1c472e0899a82cce083f
Jenn Greiner <jenn.greiner1@gmail.com>
2021-01-21 00:06:13 -0700

    index.html elements hide/display based on user actions & timer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,24 +1,24 @@
 //Javascript quiz
 
 //define variables
-var startButton = document.getElementById("start-button")
-var clearScoresButton = document.getElementById("clear-scores")
-var returnButton = document.getElementById("return")
-var saveButton = document.getElementById("save")
+var startButton = document.getElementById("start-button");
+var clearScoresButton = document.getElementById("clear-scores");
+var returnButton = document.getElementById("return");
+var saveButton = document.getElementById("save");
 var timeEl = document.getElementById("time");
 var questionEl = document.getElementById("question");
 var optionEl = document.querySelector(".options");
 var listItem = document.querySelector("li");
-var playerInitials = document.getElementById("initials")
+var playerInitials = document.getElementById("initials");
 var secondsLeft = 10;
 
 // main index
 // When my browser opens, I see View highscores button top left, Timer top right, and Coding Quiz Challenge, overview, and start game button on page. Questions are hidden
 
 // hide all elements with class = hide on start screen
-// [].forEach.call(document.querySelectorAll('.hide'), function (el) {
-//     el.style.visibility = 'hidden';
-// });
+[].forEach.call(document.querySelectorAll('.hide'), function (el) {
+    el.style.visibility = 'hidden';
+});
 
 // timer
 function setTime() {
@@ -46,33 +46,46 @@ function setTime() {
 // Function to end game and open score screen
 function endGame() {
     timeEl.textContent = " ";
+
+    // hide questions screen
+    [].forEach.call(document.querySelectorAll('#question-block'), function (el) {
+        el.style.visibility = 'hidden';
+    });
+
     // display end screen
-    // document.getElementById('feedback').style.visibility = 'visible';
+    document.getElementById('end-screen').style.visibility = 'visible';
 
     // display final score
     document.getElementById("final-score").append(secondsLeft);
 
-    // save score when submit button clicked
+    // When I click Save, my intials and score are stored 
     saveButton.addEventListener("click", saveScore)
+    // When I click Save, Highscores.html opens
+    // saveButton.addEventListener("click", )
 };
-
-
 
 
 // When i click Start Game button, my html displays the first question with answer options, and the clock on the timer starts to run down.
 startButton.addEventListener("click", function startGame() {
     // start timer
     setTime();
-    // display first question
-    // displayQuestion();
+    // hide start screen
+    [].forEach.call(document.querySelectorAll('.start'), function (el) {
+        el.style.visibility = 'hidden';
+    });
+    // display Questions
+    displayQuestion();
 });
 
 function displayQuestion() {
+    document.getElementById('question-block').style.visibility = 'visible';
     const qBlock = document.getElementById("question-block");
     qBlock.innerHTML = "Question 1<br />Answer 1<br />Answer 2<br />Answer 3"
+    // if secondsLeft=0, endGame
+    if (secondsLeft === 0) {
+        endGame();
+    };
 };
-
-
 
 // When I click an answer choice, I am prompted with feedback that i answered the question correctly or not. Then I am presented another question.
 // if correct = true {"Correct!"} else {"Wrong!"}
@@ -86,14 +99,13 @@ if (secondsLeft === 0) {
 };
 
 // final score displayed after all questions are answered
-// final score is the number of seconds remaining on the timer
-// enter initials in text field to save score
 
-// When I click Save, my intials and score are stored and Highscores.html opens
 // Save scores
 function saveScore() {
     var playerScore = {
+        // enter initials in text field to save score
         initials: playerInitials.value,
+        // final score is the number of seconds remaining on the timer
         score: secondsLeft,
     }
     // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
