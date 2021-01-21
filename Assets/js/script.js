bb86eb9e1deb4fe4a79cc8f7a1e41893e8d08061
Jenn Greiner <jenn.greiner1@gmail.com>
2021-01-20 18:20:55 -0700

    questions and initials html; js pseudocode

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,17 +1,18 @@
 //Javascript quiz
 
 //define variables
-var startButton = document.querySelector(".start-button")
-var clearScoresButton = document.querySelector(".clear-scores")
-var returnButton = document.querySelector(".return")
+var startButton = document.querySelector("#start-button")
+var clearScoresButton = document.querySelector("#clear-scores")
+var returnButton = document.querySelector("#return")
 var timeEl = document.querySelector(".time");
 var questionEl = document.querySelector(".question");
 var optionEl = document.querySelector(".options");
 var listItem = document.querySelector("li");
+var playerInitials = document.querySelector("#initials")
 var secondsLeft = 30;
 
 // main index
-// When my browser opens, I see View highscores button top left, Timer top right, and Coding Quiz Challenge, overview, and start game button on page.
+// When my browser opens, I see View highscores button top left, Timer top right, and Coding Quiz Challenge, overview, and start game button on page. Questions are hidden
 
 
 // timer
@@ -30,7 +31,7 @@ function setTime() {
     }, 1000);
 };
 
-// Function to create and append colorsplosion image
+// Function to end game and open score screen
 function endGame() {
     timeEl.textContent = " ";
     // display scores
@@ -43,21 +44,38 @@ startButton.addEventListener("click", function () {
     // start timer
     setTime();
     // display first question
+    displayQuestion();
 });
 
+function displayQuestion() {
+    questionEl. // how do you show something?
+};
 
 
-    // When I click an answer choice, I am prompted with feedback that i answered the question correctly or not. Then I am presented another question.
-        // if correct = true {"Correct!"} else {"Wrong!"}
 
-    // When I give a wrong answer, the timer decreases by 10 seconds.
-        // if correct != true {timer i-10}
+// When I click an answer choice, I am prompted with feedback that i answered the question correctly or not. Then I am presented another question.
+// if correct = true {"Correct!"} else {"Wrong!"}
 
-    // When i answer all the questions OR the time runs out, the game ends and I am taken to the score screen.
-        // final score displayed after all questions are answered
-        // final score is the number of seconds remaining on the timer
-        // enter initials in text field to save score
-        // When I click Save, my intials and score are stored and Highscores.html opens
+// When I give a wrong answer, the timer decreases by 10 seconds.
+// if correct != true {timer i-10}
+
+// When i answer all the questions OR the time runs out, the game ends and I am taken to the score screen.
+if (secondsLeft === 0 || questionsLeft === 0) {
+    endGame();
+};
+
+// final score displayed after all questions are answered
+// final score is the number of seconds remaining on the timer
+// enter initials in text field to save score
+
+// When I click Save, my intials and score are stored and Highscores.html opens
+// Save scores
+function saveScore() {
+    var playerScore = {
+        initials: playerInitials,
+        score: secondsLeft,
+    }
+}
 
 // Highscores live in separate HTML page; displays list of saved scores
     // Scores are stored even if you refresh your browser
