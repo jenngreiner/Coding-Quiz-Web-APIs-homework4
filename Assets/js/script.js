b84c139d4563b9c2eca0bf18f5443f88092c0f7d
Jenn Greiner <jenn.greiner1@gmail.com>
2021-01-24 11:28:37 -0700

    Questions cycle & grade checker, new js for highscores page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,15 +2,13 @@
 
 //define variables
 var startButton = document.getElementById("start-button");
-var clearScoresButton = document.getElementById("clear-scores");
-var returnButton = document.getElementById("return");
 var saveButton = document.getElementById("save");
 var timeEl = document.getElementById("time");
 var questionEl = document.getElementById("question");
 var optionEl = document.querySelector(".options");
 var listItem = document.querySelector("li");
-var playerInitials = document.getElementById("initials");
-var secondsLeft = 4;
+var qBlock = document.getElementById("question-block");
+var secondsLeft = 60;
 
 // question variables
 var question = [
@@ -44,23 +42,19 @@ var question = [
         choices: ["alert", "confirm", "input", "prompt",],
         answer: "prompt",
     },
-
-]
-
+];
+var questionIndex = 0;
 // main index
 // When my browser opens, I see View highscores button top left, Timer top right, and Coding Quiz Challenge, overview, and start game button on page. Questions are hidden
-
-// hide all elements with class = hide on start screen
 [].forEach.call(document.querySelectorAll('.hide'), function (el) {
     el.style.visibility = 'hidden';
 });
-
 // timer
 function setTime() {
     // Sets interval in variable
     var timerInterval = setInterval(function () {
         secondsLeft--;
-        timeEl.textContent = secondsLeft;
+        timeEl.textContent = "Time remaining: " + secondsLeft;
 
         if (secondsLeft === 0) {
             // Stops execution of action at set interval
@@ -69,106 +63,140 @@ function setTime() {
             endGame();
         }
     }, 1000);
+    render
 };
-
-// setTimeout(() => {
-//     const highScore = document.getElementById("high-score");
-//     highScore.textContent = "secondsLeft";
-//     const qBlock = document.getElementById("question-block");
-//     qBlock.innerHTML = "Question 1<br />Answer 1<br />Answer 2<br />Answer 3"
-// }, 3000);
-
-// Function to end game and open score screen
 
 // function to display final score and get player initials once all questions are answered or secondsLeft is 0
 function endGame() {
-    timeEl.textContent = " ";
-
+    // clear timer
+    timeEl.textContent = "Time's Up!";
     // hide questions screen
-    [].forEach.call(document.querySelectorAll('#question-block'), function (el) {
-        el.style.visibility = 'hidden';
-    });
-
-    // display end screen
+    qBlock.innerHTML = "";
+    // display end screen    
     document.getElementById('end-screen').style.visibility = 'visible';
 
     // display final score
     document.getElementById("final-score").append(secondsLeft);
 
     // When I click Save, my intials and score are stored 
-    saveButton.addEventListener("click", saveScore)
-    // When I click Save, Highscores.html opens
-    saveButton.addEventListener("click", renderHighscores)
+    saveButton.addEventListener("click", function () { saveScore(document.getElementById("initials").value, secondsLeft) })
 };
 
 // When i click Start Game button, my html displays the first question with answer options, and the clock on the timer starts to run down.
-startButton.addEventListener("click", function startGame() {
-    // start timer
-    setTime();
-    // hide start screen
-    [].forEach.call(document.querySelectorAll('.start'), function (el) {
-        el.style.visibility = 'hidden';
+if (startButton) {
+    startButton.addEventListener("click", function startGame() {
+        // start timer
+        setTime();
+        // display Questions
+        render(questionIndex);
     });
-    // display Questions
-    displayQuestion();
-});
+}
 
-function displayQuestion() {
-    document.getElementById('question-block').style.visibility = 'visible';
-    const qBlock = document.getElementById("question-block");
-    qBlock.innerHTML = "Question 1<br />Answer 1<br />Answer 2<br />Answer 3"
-    // how do you insert questions here?
-    // <div id="question-block" class="hide">
-    //         <h2 id="question-title"></h2>
-    //         <div id="choices" class="choices"></div>
-    //     </div>
 
+function render(questionIndex) {
+    // creates new ul element
+    var ulCreate = document.createElement("ul");
+    // Clears question-block div content (hides start screen)
+    qBlock.innerHTML = "";
+    ulCreate.innerHTML = "";
+
+    // For loop to loop throug question array
+    for (var i = 0; i < question.length; i++) {
+        // Append question title
+        var qTitle = question[questionIndex].title;
+        var qChoice = question[questionIndex].choices;
+        // console.log(qTitle);
+        // console.log(qChoice);
+        qBlock.textContent = qTitle;
+    }
+    // for each for questions choice
+    qChoice.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        qBlock.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
     // if secondsLeft=0, endGame
     if (secondsLeft === 0) {
         endGame();
     };
 };
 
-// When I click an answer choice, I am prompted with feedback that i answered the question correctly or not. Then I am presented another question.
-// if correct = true {"Correct!"} else {"Wrong!"}
+function compare(event) {
+    // save user choice as element
+    var userChoice = event.target
+    // compare user choice with answer
+    if (userChoice.matches("li")) {
+        var createDiv = document.createElement("div")
+        createDiv.setAttribute("id", "createDiv")
 
-// When I give a wrong answer, the timer decreases by 10 seconds.
-// if correct != true {timer i-10}
+        // Correct answer
+        if (userChoice.textContent === question[questionIndex].answer) {
+            createDiv.innerHTML = "Correct!"
+        } else {
+            // wrong answer; will deduct 5 seconds from score
+            secondsLeft = secondsLeft - 5
+            createDiv.innerHTML = "Wrong! The correct answer is: " + question[questionIndex].answer;
+        }
+    }
+    // determine which question the user is one, end if all questions answered
+    questionIndex++;
+    // When i answer all the questions OR the time runs out, the game ends and I am taken to the score screen.
 
-// When i answer all the questions OR the time runs out, the game ends and I am taken to the score screen.
-if (secondsLeft === 0) {
-    endGame();
-};
+    if (questionIndex >= question.length || secondsLeft === 0) {
+        // end game after last question
+        endGame();
+    } else {
+        render(questionIndex);
+    }
+    qBlock.appendChild(createDiv);
+
+}
 
 // Save scores
 var playerScore = "";
 
-function saveScore() {
-    var playerScore = {
-        // enter initials in text field to save score
-        initials: playerInitials.value,
-        // final score is the number of seconds remaining on the timer
-        score: secondsLeft,
-    };
+function saveScore(initials, score) {
+
+    // console.log(initials);
+    // console.log(score);
+    var playerScores = JSON.parse(localStorage.getItem("playerScores"));
+    // uses initials as the key to look up the score
+    if (!playerScores) {
+        playerScores = {}
+    }
+    // playerScores[initials] = Math.max(playerScores[initials], score)
+    if (playerScores[initials]) {
+        if (playerScores[initials] < score) {
+            playerScores[initials] = score
+        }
+    } else {
+        playerScores[initials] = score
+    }
+    // playerScores[initials] = score > parseInt(playerScores[initials]) ? score : playerScores[initials];
     // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-    localStorage.setItem("playerScore", JSON.stringify(playerScore));
-    console.log(playerScore);
-    console.log(playerScore.score)
-}
+    // console.log(JSON.stringify(playerScores));
+
+    localStorage.setItem("playerScores", JSON.stringify(playerScores));
+    // location redirect // When I click Save, Highscores.html opens
+    location.href = "highscores.html"
+};
 
 // Highscores live in separate HTML page; displays list of saved scores
 function renderHighscores() {
     // Use JSON.parse() to convert text to JavaScript object
-    var highScore = JSON.parse(localStorage.getItem("playerScore"));
-    console.log(highScore);
-    // Check if data is returned, if not exit out of the function
-    if (highScore != null) {
-        document.getElementById("score-item").innerHTML = highScore.initials;
-    } else {
-        console.log("null");
-        return;
+    var playerScores = JSON.parse(localStorage.getItem("playerScores"));
+    var scoreList = "";
+    for (x in playerScores) {
+        scoreList += x + " : " + playerScores[x] + "<br>"
     }
+    document.getElementById("score-item").innerHTML = scoreList
 };
-    // Scores are stored even if you refresh your browser
-    // When I click go back, index.html opens
-    // When I click Clear Highscores, all score data is erased
+
+
+// When I click Go Back, index.html opens
+function goBack() {
+    // open index.html
+    window.location.replace("index.html");
+}
